c93ed3803f553ec2f6295c8996f4c3e061afa0c7
Jay Indino <45135991+jaydesigns@users.noreply.github.com>
2023-02-06 15:24:19 -0700

    Added motion in accordion

--- components/workGallery.js
+++ components/workGallery.js
@@ -28,31 +28,27 @@ const Card = ({title,coverImg,category,imgWidth}) => {
 const Accordion = ({title,skills}) => {
     const dropdownItems = useRef()
     const plusSign = useRef()
-    const plustl = useRef()
-    const [state,setState] = useState(false)
-
-    useIsomorphicLayoutEffect(()=>{
-        gsap.set(dropdownItems.current.querySelectorAll("span"),{translateY:"-100%"})
-        gsap.set(dropdownItems.current.querySelectorAll(".skills"),{height:"0em"})
-    })
+    const [isOpen,setIsOpen] = useState(true)
+    const plustl = useRef(gsap.timeline())
 
     const handleExpand = () => {
-        plustl.current = gsap.timeline()
-        .to(plusSign.current,{rotate:"135deg"})
-        .to(dropdownItems.current.querySelectorAll(".skills"),{height:"1.5em"},"<")
-        setState(true)
-        plustl.current.play()
+        setIsOpen(!isOpen)
     }
 
-    const handleContract = () => {
-        plustl.current.reverse()
-        setState(false)
-    }
+    useIsomorphicLayoutEffect(()=>{
+        plustl.current.fromTo(plusSign.current,{rotate:"0deg"},{rotate:"135deg"})
+        plustl.current.fromTo(dropdownItems.current.querySelectorAll(".skills"),{width:0,height:"0em"},{width:"100%",height:"1.5em",stagger:0.1,ease:"power3.inOut",duration:1.2},"<")
+        plustl.current.fromTo(dropdownItems.current.querySelectorAll(".workSelector"),{translateY:"-120%"},{translateY:"0%",stagger:0.05,duration:1.2,ease:"power3.inOut"},"<")
+    },[])
+
+    useIsomorphicLayoutEffect(()=>{
+        isOpen?plustl.current.reverse():plustl.current.play()
+    },[isOpen])
 
     return (
         <div className="flex-1">
-            <div className="flex flex-col border-t border-b border-white py-2">
-                <div onClick={state !== true ? handleExpand : handleContract} className="cursor-pointer flex gap-4 justify-start">
+            <div className="flex flex-col border-b border-white py-2">
+                <div onClick={handleExpand} className="cursor-pointer flex gap-4 justify-start">
                     <div className="relative w-5 h-5">
                         <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]" ref={plusSign}><span className="text-2xl">+</span></div>
                     </div>
@@ -61,7 +57,7 @@ const Accordion = ({title,skills}) => {
                 <div ref={dropdownItems} className="flex flex-col">
                     {skills.map((el,i)=>{
                         return (
-                        <div key={i} className="overflow-hidden skills"><span className="pl-10 text-sm cursor-pointer workSelector underline">{el}</span></div>
+                        <div key={i} className="relative overflow-hidden skills border-grey border-t"><span className="absolute pl-10 text-sm cursor-pointer workSelector">{el}</span></div>
                         )
                     })}
                 </div>
@@ -72,17 +68,22 @@ const Accordion = ({title,skills}) => {
 
 const WorkGallery = () => {
     const images = useRef();
+
     useIsomorphicLayoutEffect(()=>{
         gsap.registerPlugin(ScrollTrigger)
-        gsap.timeline()
-        .set(images.current.querySelectorAll("img"),{clipPath:"inset(100% 0% 0% 0%)"})
-        .to(images.current.querySelectorAll("img"),{clipPath:"inset(0% 0% 0% 0%)",stagger: 0.2, scrollTrigger:{scroller:"body",trigger:"#selectedWork", start:"55% bottom", end:"top top", scrub:true,pinSpacing:false}})
-        .to(images.current.querySelectorAll("img"),{clipPath:"inset(0% 0% 100% 0%)",stagger: 0.2, scrollTrigger:{scroller:"body",trigger:"#selectedWork", start:"bottom bottom", end:"center top", scrub:true,pinSpacing:false}})
+        const tl = gsap.timeline()
+        tl.set(images.current.querySelectorAll("img"),{clipPath:"inset(100% 0% 0% 0%)"})
+        tl.fromTo(images.current.querySelectorAll("img"),{clipPath:"inset(100% 0% 0% 0%)"},{clipPath:"inset(0% 0% 0% 0%)",stagger: 0.1, scrollTrigger:{scroller:"body",trigger:"#selectedWork", start:"75% bottom", end:"top top", scrub:true,pinSpacing:false}})
+    },[])
+
+    useIsomorphicLayoutEffect(()=>{
+        gsap.registerPlugin(ScrollTrigger)
+        gsap.fromTo(images.current.querySelectorAll("img"),{clipPath:"inset(0% 0% 0% 0%)"},{clipPath:"inset(0% 0% 100% 0%)",stagger: 0.1, scrollTrigger:{scroller:"body",trigger:"#selectedWork", start:"bottom bottom", end:"center top", scrub:true,pinSpacing:false}})
     },[])
 
     return(
         <div id="selectedWork" className="snap-start flex flex-col text-white selectedWork w-full h-screen p-4 pt-8 pb-24 justify-between">
-            <div className="flex flex-col border-grey border-t md:flex-row">
+            <div className="flex flex-col border-grey border-t border-b md:border-black md:flex-row">
                 <h1 className="flex-1 tracking-tight leading-suis text-4xl font-smibold py-2">Selected Work</h1>
                 <div className="flex flex-col md:grow md:flex-row">
                     <Accordion title={'Type of Work'} skills={['Branding', 'UI Design', 'Environmental Design', 'Publication Design']}/>
