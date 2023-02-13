cbe7d92040964ddef1a39cf1cd55f64eec645de3
Jay Indino <45135991+jaydesigns@users.noreply.github.com>
2023-02-12 17:41:56 -0700

    Disabled parallax for mobile in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ import Contact from '../components/contact'
 const inter = Inter({ subsets: ['latin'] })
 
 const Home =({heading1Wrapper,heading2Wrapper,ampersandWrapper})=> {
+  const mm = useRef(gsap.matchMedia())
   //const smoother = useContext(SmootherContext)
   //Register GSAP Plugin - ScrollTrigger
   //gsap.registerPlugin(ScrollTrigger);
@@ -33,7 +34,7 @@ const Home =({heading1Wrapper,heading2Wrapper,ampersandWrapper})=> {
       gsap.fromTo(".changeBG",{backgroundColor:"#141011"},{backgroundColor:"#DFE0E2",scrollTrigger:{scroller:"body",trigger:".interactiveContact",scrub:true , start:"top 50%", end:"top top", pinSpacing:false}})
       gsap.fromTo(".changeBG",{backgroundColor:"#DFE0E2"},{backgroundColor:"#141011",scrollTrigger:{scroller:"body",trigger:".selectedWork",scrub:true , start:"top bottom", end:"top center", pinSpacing:false}})
       gsap.to([".entra",ampersandWrapper.current],{translateY:"-150%",stagger:0.05,scrollTrigger:{scroller:"body",trigger:".introBody",start:"bottom bottom",end:"bottom center",scrub:true,pinSpacing:false}})
-      gsap.to(".heroImage",{translateY:"32.5%", scrollTrigger:{scroller:"body",trigger:".introBody",scrub:true , start:"top top", end:"bottom center", pinSpacing:false}})
+      mm.current.add("(min-width: 768px)",()=>{gsap.to(".heroImage",{translateY:"32.5%", scrollTrigger:{scroller:"body",trigger:".introBody",scrub:true , start:"top top", end:"bottom center", pinSpacing:false}})})
     })
     return () => ctx.revert()
   },[])
